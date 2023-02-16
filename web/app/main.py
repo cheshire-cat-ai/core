c6a9171d2477c85b9ea5fc9f551a533c8aa5a371
pieroit <piero.savastano@gmail.com>
2023-02-16 01:06:31 +0100

    move frontend into its own root folder; fix main collection bug

--- web/app/main.py
+++ web/app/main.py
@@ -1,8 +1,9 @@
 import time
+from pprint import pprint
 
 from typing import Union
 
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
 
 from langchain.vectorstores import Qdrant
@@ -11,9 +12,7 @@ from qdrant_client import QdrantClient
 from qdrant_client.http.models import Distance, VectorParams
 
 import openai
-openai.api_key = 'INSERT YOUR KEY HERE'
-
-app = FastAPI()
+openai.api_key = 'INSERT KEY HERE'
 
 
 def embed(text):
@@ -23,77 +22,65 @@ def embed(text):
         model="text-embedding-ada-002"
     )
 
-    return embedding["data"][0]["embedding"]
+    return embedding["data"][0]["embedding"] 
 
 
 qdrant_c = QdrantClient(host="vector-memory", port=6333)
-qdrant_c.recreate_collection(
-    collection_name='utterances',
-    vectors_config=VectorParams(size=1536, distance=Distance.COSINE),
-)
+main_collection_name = 'utterances'
+
+try:
+    main_collection = qdrant_c.get_collection(main_collection_name)
+    print('@@@ Main collection status:')
+    pprint(main_collection.dict())
+except:
+    print('@@@ Creating main collection', qdrant_c.get_collections())
+    qdrant_c.recreate_collection(
+        collection_name=main_collection_name,
+        vectors_config=VectorParams(size=1536, distance=Distance.COSINE),
+    )
 
 vector_memory = Qdrant(
     qdrant_c,
-    'utterances',
+    main_collection_name,
     embedding_function=embed
 )
 
+#### API endpoints
 
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var ws = new WebSocket("ws://localhost:80/ws");
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
-</html>
-"""
+app = FastAPI()
 
 
-@app.get("/")
+@app.get("/") 
 def home():
-    return HTMLResponse(html)
+    return {
+        'status': 'ok'
+    }
 
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        message = await websocket.receive_text()
-        vector_ids = vector_memory.add_texts(
-            [message],
-            [{
-                'who' : 'user',
-                'when': time.time(),
-                'text': message, 
-            }]
-        )
-        #data = embed(data)
-        await websocket.send_text(f'\nReceived: {vector_ids}')
 
-        episodes = vector_memory.similarity_search(message) # TODO: why embed twice?
-        for episode in episodes:
-            await websocket.send_text(episode.page_content)
+    await websocket.accept()
+    try:
+        while True:
+            message = await websocket.receive_text()
+            print('@@@@', message)
+
+            vector_ids = vector_memory.add_texts(
+                [message],
+                [{
+                    'who' : 'user',
+                    'when': time.time(),
+                    'text': message, 
+                }]
+            )
+            
+            await websocket.send_text(f'Received: {vector_ids}')
+
+            episodes = vector_memory.similarity_search(message) # TODO: why embed twice?
+            for episode in episodes:
+                await websocket.send_text(episode.page_content)
+
+    except WebSocketDisconnect:
+        print('@@ close connection')
+        #del qdrant_c
