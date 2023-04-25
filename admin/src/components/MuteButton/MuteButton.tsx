7a459809415493fba91a7ffa476664911fd23904
PaperBackPear3 <fabioangeloni2001@gmail.com>
2023-04-25 10:35:17 +0200

    eslint fixes

--- admin/src/components/MuteButton/MuteButton.tsx
+++ admin/src/components/MuteButton/MuteButton.tsx
@@ -1,35 +1,35 @@
-import { ButtonHTMLAttributes, FC } from "react";
+import React from 'react'
+import { type ButtonHTMLAttributes, type FC } from 'react'
 import VolumeEnabled from './volume-high-solid.svg'
 import VolumeDisabled from './volume-xmark-solid.svg'
 import style from './SoundButton.module.scss'
-import clsx from "clsx";
-
+import clsx from 'clsx'
 
 /**
  * button to mute or unmute the whole app
- *  
+ *
  */
 
 const MuteButton: FC<MuteButtonProps> = (props) => {
-    const { active, onClick, className } = props
-    const classList = clsx(style.soundBtn, className)
+  const { active, onClick, className } = props
+  const classList = clsx(style.soundBtn, className)
 
-    return (
+  return (
         <button role="button" onClick={onClick} className={classList}>
             {active ? <VolumeEnabled /> : <VolumeDisabled />}
         </button>
-    )
+  )
 }
 
 export interface MuteButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-    /**
+  /**
      * Indicates whether the button is currently active (clicked).
      */
-    active?: boolean
-    /**
+  active?: boolean
+  /**
      * A callback that will be fired when the button is clicked.
      * This callback is generally used to toggle the state of the component.
      */
-    onClick?: React.MouseEventHandler<HTMLButtonElement>
+  onClick?: React.MouseEventHandler<HTMLButtonElement>
 }
 export default MuteButton
