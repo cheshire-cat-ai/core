1fa3f520987aaa6d77fade26cf3b3322a046935e
Dany <zdanymc@gmail.com>
2023-05-15 20:19:06 +0200

    Update Header.vue

--- admin/src/components/Header.vue
+++ admin/src/components/Header.vue
@@ -1,8 +1,4 @@
 <script setup lang="ts">
-import { useNotifications } from '@stores/useNotifications'
-import { uniqueId } from '@utils/commons'
-
-const { showNotification } = useNotifications()
 </script>
 
 <template>
@@ -46,7 +42,7 @@ const { showNotification } = useNotifications()
 		</div>
 		<div class="navbar-center">
 			<RouterLink to="/">
-				<img src="@assets/logo.svg" class="h-10 w-10 cursor-pointer md:hidden" @click="onLogoClick">
+				<img src="@assets/logo.svg" class="h-10 w-10 cursor-pointer md:hidden">
 			</RouterLink>
 			<ul class="menu menu-compact menu-horizontal hidden gap-2 px-1 font-semibold md:flex">
 				<li>
