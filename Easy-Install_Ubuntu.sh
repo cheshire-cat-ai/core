9bd1212bed57057719d4fa50b9eb72f230947a2a
Luca <43339283+luca1844@users.noreply.github.com>
2023-05-16 18:19:23 +0200

    Update Easy-Install_Ubuntu.sh
    Add Network requirements

--- Easy-Install_Ubuntu.sh
+++ Easy-Install_Ubuntu.sh
@@ -3,17 +3,21 @@
 ###################################################
 # 00 - Prerequisites
 ###################################################
+sudo cat << 'EOF' >> /etc/resolv.conf
+nameserver 1.1.1.1
+nameserver 1.0.0.1
+EOF
+
 sudo apt-get -y update && sudo apt-get -y upgrade
 sudo apt-get -y install gcc make perl
 sudo apt-get -y install git
 
-sudo apt-get -y install snapd
-sudo snap install core; sudo snap refresh core
-
-sudo snap install docker
+#sudo apt-get -y install snapd
+#sudo snap install core; sudo snap refresh core
+#sudo snap install docker
 
 sudo apt-get -y install docker-compose 
-sudo apt-get upgrade docker-compose
+sudo apt-get -y upgrade docker-compose
 
 
 
@@ -29,6 +33,8 @@ cp .env.example .env
 sudo docker-compose down
 git pull origin main
 sudo docker-compose build --no-cache
+
+
 sudo docker-compose up
 
 
