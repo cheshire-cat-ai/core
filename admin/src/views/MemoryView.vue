4e97a21e96bb93de0b2e72dc7444a281b6294c6a
pieroit <piero.savastano@gmail.com>
2023-05-18 01:16:47 +0200

    align memory view to others

--- admin/src/views/MemoryView.vue
+++ admin/src/views/MemoryView.vue
@@ -3,7 +3,11 @@
 </script>
 
 <template>
-	<div class="flex flex-col items-center justify-center text-center">
-		Memory (coming soon)
+	<div class="flex flex-col gap-8 self-center md:w-3/4">
+		<div class="col-span-2 flex flex-col items-center justify-center gap-3 rounded p-6">
+			<p class="text-3xl font-bold text-primary">
+				Memory (coming soon)
+			</p>
+		</div>
 	</div>
 </template>
