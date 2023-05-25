ba495d12dd27b8cca80808a9c00221ffbe392a0a
Nicola <nicorb932@hotmail.com>
2023-05-25 20:22:07 +0200

    - documented `agent.py`

--- core/cat/mad_hatter/core_plugin/hooks/agent.py
+++ core/cat/mad_hatter/core_plugin/hooks/agent.py
@@ -1,18 +1,40 @@
+"""Hooks to modify the Cat's *Agent*.
+
+Here is a collection of methods to hook into the *Agent* execution pipeline.
+
+Example:
+
+    from cat.mad_hatter.decorators import tool, hook
+
+    @hook(priority=1)
+
+    def name_of_the_hook_to_overridden(use-equal-parameters):
+        **custom execution**
+
+        return custom_output
+
+"""
+
+from typing import List
+
+from langchain.tools.base import BaseTool
 from langchain.agents import load_tools
 from cat.mad_hatter.decorators import hook
 
 
 @hook(priority=0)
-def agent_allowed_tools(tools, cat):
-    """Decide which tools end up in the agent prompt. To decide you can filter the list of loaded tools,
-        but you can also check the context in cat.working_memory and launch custom chains with cat.llm.
+def agent_allowed_tools(tools: List[BaseTool], cat) -> List[BaseTool]:
+    """Hook the allowed tools.
+
+    Decide which tools end up in the *Aget* prompt. To decide, you can filter the list of loaded tools,
+    but you can also check the context in `cat.working_memory` and launch custom chains with `cat.llm`.
 
     Args:
-        tools: list of @tool functions extracted from plugins
-        cat: instance of the cat
+        tools: list of @tool functions extracted from the plugins
+        cat: instance of the Cat
 
     Returns:
-        list of allowed tools
+        List of allowed tools
     """
 
     # add to plugin defined tools, also some default tool included in langchain
