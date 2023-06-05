d7ce1a5fc7d1b92dfd875cde7d83ffa3245ce265
pieroit <piero.savastano@gmail.com>
2023-06-05 12:13:45 +0200

    fix #263: mount admin and static files in separate files

--- core/cat/main.py
+++ core/cat/main.py
@@ -1,17 +1,14 @@
 import os
-import re
-import json
 from contextlib import asynccontextmanager
 
 from fastapi import Depends, FastAPI
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
 from fastapi.responses import JSONResponse
 from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.cors import CORSMiddleware
 
 from cat.log import log
 from cat.routes import base, memory, plugins, upload, websocket
+from cat.routes.static import chat, admin, static
 from cat.api_auth import check_api_key
 from cat.routes.openapi import get_openapi_configuration_function
 from cat.routes.setting import llm_setting, general_setting, embedder_setting
@@ -62,38 +59,16 @@ cheshire_cat_api.include_router(upload.router, tags=["Rabbit Hole (file upload)"
 cheshire_cat_api.include_router(websocket.router, tags=["Websocket"])
 
 
-# admin index.html
-# all the admin files are served in a statci way
-# with the exception of index.html that needs a config file derived from core environments variable:
-#  - CORE_HOST
-#  - CORE_PORT
-#  - API_KEY
-@cheshire_cat_api.get("/admin/")
-def admin_index_injected():
-    cat_core_config = json.dumps({
-        "CORE_HOST": os.getenv("CORE_HOST"),
-        "CORE_PORT": os.getenv("CORE_PORT"),
-        "CORE_USE_SECURE_PROTOCOLS": os.getenv("CORE_USE_SECURE_PROTOCOLS"),
-        "API_KEY": os.getenv("API_KEY"),
-    })
+# mount static files
+# this cannot be done via fastapi.APIrouter:
+# https://github.com/tiangolo/fastapi/discussions/9070
 
-    with open("/admin/dist/index.html", 'r') as f:
-        html = f.read()
-
-    # TODO: this is ugly, should be done with beautiful soup or a template
-    regex = re.compile(r"catCoreConfig = (\{.*?\})", flags=re.MULTILINE | re.DOTALL)
-    default_config = re.search(regex, html).group(1)
-    html = html.replace(default_config, cat_core_config)
-
-    return HTMLResponse(html)
-
-
-# admin (all static files)
-# note html=False because index.html needs to be injected with runtime information
-cheshire_cat_api.mount("/admin", StaticFiles(directory="/admin/dist/", html=False), name="admin")
-
+# admin (index file with dynamic configuration)
+admin.mount_admin_index(cheshire_cat_api)
+# admin (static build)
+admin.mount(cheshire_cat_api)
 # static files (for plugins and other purposes)
-cheshire_cat_api.mount("/static", StaticFiles(directory="cat/static"), name="static")
+static.mount(cheshire_cat_api)
 
 
 # error handling
---
+++ core/cat/routes/static/admin.py
@@ -0,0 +1,48 @@
+import os
+import re
+import json
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+
+
+def mount(cheshire_cat_api):
+
+    mount_admin_index(cheshire_cat_api)
+
+    # note html=False because index.html needs to be injected with runtime information
+    cheshire_cat_api.mount(
+        "/admin", StaticFiles(directory="/admin/dist/", html=False), name="admin")
+
+
+def mount_admin_index(cheshire_cat_api):
+
+    @cheshire_cat_api.get("/admin/")
+    def admin_index_injected():
+        # admin index.html
+        # all the admin files are served in a statci way
+        # with the exception of index.html that needs a config file derived from core environments variable:
+        #  - CORE_HOST
+        #  - CORE_PORT
+        #  - API_KEY
+        cat_core_config = json.dumps({
+            "CORE_HOST": os.getenv("CORE_HOST"),
+            "CORE_PORT": os.getenv("CORE_PORT"),
+            "CORE_USE_SECURE_PROTOCOLS": os.getenv("CORE_USE_SECURE_PROTOCOLS"),
+            "API_KEY": os.getenv("API_KEY"),
+        })
+
+        # the admin sttic build is created during docker build from this repo:
+        # https://github.com/cheshire-cat-ai/admin-vue
+        # the files live inside the /admin folder (not visible in volume / cat code)
+        with open("/admin/dist/index.html", 'r') as f:
+            html = f.read()
+
+        # TODO: this is ugly, should be done with beautiful soup or a template
+        regex = re.compile(
+            r"catCoreConfig = (\{.*?\})", flags=re.MULTILINE | re.DOTALL)
+        default_config = re.search(regex, html).group(1)
+        html = html.replace(default_config, cat_core_config)
+
+        return HTMLResponse(html)
+
+
