90381a9addd33e13d6c9482064041284f511156b
Dany <zdanymc@gmail.com>
2023-06-16 20:29:41 +0200

    added new endpoints, updated plugin metadata

--- core/cat/routes/plugins.py
+++ core/cat/routes/plugins.py
@@ -1,4 +1,4 @@
-from fastapi import Request, APIRouter, HTTPException
+from fastapi import Request, APIRouter, HTTPException, UploadFile, BackgroundTasks
 from typing import Dict
 
 router = APIRouter()
@@ -15,14 +15,32 @@ async def list_available_plugins(request: Request) -> Dict:
     # plugins are managed by the MadHatter class a = b if b else val
     plugins = ccat.mad_hatter.plugins or []
 
+    # retrieve plugins from official repo
+    registry = []
+
     return {
         "status": "success", 
-        "results": len(plugins), 
-        "plugins": plugins
+        "results": len(plugins) + len(registry), 
+        "installed": plugins,
+        "registry": registry
     }
 
 
-@router.get("/toggle/{plugin_id}", status_code=200)
+@router.post("/install/")
+async def install_plugin(
+    request: Request,
+    file: UploadFile,
+    background_tasks: BackgroundTasks
+) -> Dict:
+    """Install a new plugin from a zip file"""
+
+    # access cat instance
+    ccat = request.app.state.ccat
+
+    return {"error": "to be implemented"}
+
+
+@router.put("/toggle/{plugin_id}", status_code=200)
 async def toggle_plugin(plugin_id: str, request: Request) -> Dict:
     """Enable or disable a single plugin"""
 
