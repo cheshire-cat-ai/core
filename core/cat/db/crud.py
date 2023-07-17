2f5c027dc80e0e1fdf5ec05e624a8c366619ebf9
pieroit <piero.savastano@gmail.com>
2023-07-17 15:50:42 +0200

    call db with get_db

--- core/cat/db/crud.py
+++ core/cat/db/crud.py
@@ -3,24 +3,24 @@ from typing import Dict, List
 from tinydb import Query
 
 from cat.db import models
-from cat.db.database import Database
+from cat.db.database import get_db
 from cat.log import log
 
 
 def get_settings(search: str = "") -> List[Dict]:
     query = Query()
-    return Database().search(query.name.matches(search))
+    return get_db().search(query.name.matches(search))
 
 
 def get_settings_by_category(category: str) -> List[Dict]:
     query = Query()
-    return Database().search(query.category == category)
+    return get_db().search(query.category == category)
 
 
 def create_setting(payload: models.Setting) -> Dict:
     
     # Missing fields (setting_id, updated_at) are filled automatically by pydantic
-    Database().insert(payload.dict())
+    get_db().insert(payload.dict())
     
     # retrieve the record we just created
     new_record = get_setting_by_id(payload.setting_id)
@@ -30,7 +30,7 @@ def create_setting(payload: models.Setting) -> Dict:
 
 def get_setting_by_name(name: str) -> Dict:
     query = Query()
-    result = Database().search(query.name == name)
+    result = get_db().search(query.name == name)
     if len(result) > 0:
         return result[0]
     else:
@@ -39,7 +39,7 @@ def get_setting_by_name(name: str) -> Dict:
 
 def get_setting_by_id(setting_id: str) -> Dict:
     query = Query()
-    result = Database().search(query.setting_id == setting_id)
+    result = get_db().search(query.setting_id == setting_id)
     if len(result) > 0:
         return result[0]
     else:
@@ -48,13 +48,13 @@ def get_setting_by_id(setting_id: str) -> Dict:
 
 def delete_setting_by_id(setting_id: str) -> None:
     query = Query()
-    Database().remove(query.setting_id == setting_id)    
+    get_db().remove(query.setting_id == setting_id)    
 
 
 def update_setting_by_id(payload: models.Setting) -> Dict:
     
     query = Query()
-    Database().update(payload, query.setting_id == payload.setting_id)
+    get_db().update(payload, query.setting_id == payload.setting_id)
 
     return get_setting_by_id(payload.setting_id)
 
@@ -67,6 +67,6 @@ def upsert_setting_by_name(payload: models.Setting) -> models.Setting:
         create_setting(payload)
     else:
         query = Query()
-        Database().update(payload, query.name == payload.name)
+        get_db().update(payload, query.name == payload.name)
 
     return get_setting_by_name(payload.name)
