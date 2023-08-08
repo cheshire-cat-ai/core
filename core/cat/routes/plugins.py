d017f205446950c6f3457bf64eb52b7c3b5e0e7d
coconutcow <sabharwalsanket@gmail.com>
2023-08-08 20:19:12 +0200

    Refactor plugins.py for improved readability and maintainability
    - Introduced utility function `get_ccat_instance` to simplify repeated code.
    - Enhanced route handlers to leverage the utility function.
    - Cleaned up and restructured several sections for clarity.
    
    To be done
    - Will add this utility function as part of the utils.py and later pull it multiple areas of the code

--- core/cat/routes/plugins.py
+++ core/cat/routes/plugins.py
@@ -9,9 +9,18 @@ import requests
 
 router = APIRouter()
 
+# Code Constants
+PLUGINS_URL = "https://plugins.cheshirecat.ai/plugins?page=1&page_size=7000"
+GITHUB_API_BASE = "https://api.github.com/repos"
+ADMITTED_MIME_TYPES = ["application/zip", 'application/x-tar']
+
+def get_ccat_instance(request: Request):
+    """Retrieve the ccat instance from the request."""
+    return request.app.state.ccat
+
 async def get_registry_list():
     try:
-        response = await requests.get("https://plugins.cheshirecat.ai/plugins?page=1&page_size=7000")
+        response = await requests.get(PLUGINS_URL)
         if response.status_code == 200:
             return response.json()["plugins"]
         else:
@@ -26,7 +35,7 @@ async def list_available_plugins(request: Request) -> Dict:
     """List available plugins"""
 
     # access cat instance
-    ccat = request.app.state.ccat
+    ccat = get_ccat_instance(request)
 
     active_plugins = ccat.mad_hatter.load_active_plugins_from_db()
 
@@ -57,15 +66,14 @@ async def install_plugin(
     """Install a new plugin from a zip file"""
 
     # access cat instance
-    ccat = request.app.state.ccat
+    ccat = get_ccat_instance(request)
 
-    admitted_mime_types = ["application/zip", 'application/x-tar']
     content_type = mimetypes.guess_type(file.filename)[0]
-    if content_type not in admitted_mime_types:
+    if content_type not in ADMITTED_MIME_TYPES:
         raise HTTPException(
             status_code = 400,
             detail={
-                "error": f'MIME type `{file.content_type}` not supported. Admitted types: {", ".join(admitted_mime_types)}'
+                "error": f'MIME type `{file.content_type}` not supported. Admitted types: {", ".join(ADMITTED_MIME_TYPES)}'
             },
         )
 
@@ -94,31 +102,29 @@ async def install_plugin_from_registry(
     url_repo: Dict = Body(example={"url": "https://github.com/plugin-dev-account/plugin-repo"})
     ) -> Dict:
     """Install a new plugin from external repository"""
-    
+
     # search for a release on Github
     path_url = str(urlparse(url_repo["url"]).path)
-    url = "https://api.github.com/repos" +  path_url + "/releases"
+    url = GITHUB_API_BASE +  path_url + "/releases"
     response = requests.get(url)
     if response.status_code != 200:
             raise HTTPException(
                 status_code = 503,
                 detail = { "error": "Github API not available" }
             )
-            
+
     response = response.json()
-    
-    #Check if there are files for the latest release
-    if len(response) != 0: 
+
+    # Check if there are files for the latest release
+    if len(response) != 0:
         url_zip = response[0]["assets"][0]["browser_download_url"]
     else:
         # if not, than download the zip repo
         # TODO: extracted folder still contains branch name
         url_zip = url_repo["url"] + "/archive/master.zip"
-        
+
     # Get plugin name
-    arr = path_url.split("/")
-    arr.reverse()
-    plugin_name = arr[0] + ".zip"
+    plugin_name = path_url.split("/")[-1] + ".zip"
 
     with requests.get(url_zip, stream=True) as response:
         if response.status_code != 200:
@@ -126,15 +132,15 @@ async def install_plugin_from_registry(
                 status_code = 400,
                 detail = { "error": "" }
             )
-        
+
         with NamedTemporaryFile(delete=False,mode="w+b",suffix=plugin_name) as file:
             for chunk in response.iter_content(chunk_size=8192):
                 file.write(chunk)
             log(f"Uploading plugin {plugin_name}", "INFO")
 
             #access cat instance
-            ccat = request.app.state.ccat
-            
+            ccat = get_ccat_instance(request)
+
 
             background_tasks.add_task(
                 ccat.mad_hatter.install_plugin, file.name
@@ -153,7 +159,7 @@ async def toggle_plugin(plugin_id: str, request: Request) -> Dict:
     """Enable or disable a single plugin"""
 
     # access cat instance
-    ccat = request.app.state.ccat
+    ccat = get_ccat_instance(request)
 
     # check if plugin exists
     if not ccat.mad_hatter.plugin_exists(plugin_id):
@@ -161,7 +167,7 @@ async def toggle_plugin(plugin_id: str, request: Request) -> Dict:
             status_code = 404,
             detail = { "error": "Plugin not found" }
         )
-    
+
     # toggle plugin
     ccat.mad_hatter.toggle_plugin(plugin_id)
 
@@ -176,8 +182,8 @@ async def get_plugin_details(plugin_id: str, request: Request) -> Dict:
     """Returns information on a single plugin"""
 
     # access cat instance
-    ccat = request.app.state.ccat
-    
+    ccat = get_ccat_instance(request)
+
     if not ccat.mad_hatter.plugin_exists(plugin_id):
         raise HTTPException(
             status_code = 404,
@@ -201,7 +207,7 @@ async def get_plugin_settings(request: Request, plugin_id: str) -> Dict:
     """Returns the settings of a specific plugin"""
 
     # access cat instance
-    ccat = request.app.state.ccat
+    ccat = get_ccat_instance(request)
 
     if not ccat.mad_hatter.plugin_exists(plugin_id):
         raise HTTPException(
@@ -229,18 +235,18 @@ async def upsert_plugin_settings(
     """Updates the settings of a specific plugin"""
 
     # access cat instance
-    ccat = request.app.state.ccat
+    ccat = get_ccat_instance(request)
 
     if not ccat.mad_hatter.plugin_exists(plugin_id):
         raise HTTPException(
             status_code = 404,
             detail = { "error": "Plugin not found" }
         )
-    
+
     final_settings = ccat.mad_hatter.plugins[plugin_id].save_settings(payload)
 
     return {
-        "status": "success", 
+        "status": "success",
         "settings": final_settings
     }
 
@@ -250,14 +256,14 @@ async def delete_plugin(plugin_id: str, request: Request) -> Dict:
     """Physically remove plugin."""
 
     # access cat instance
-    ccat = request.app.state.ccat
+    ccat = get_ccat_instance(request)
 
     if not ccat.mad_hatter.plugin_exists(plugin_id):
         raise HTTPException(
             status_code = 404,
             detail = { "error": "Item not found" }
         )
-    
+
     # remove folder, hooks and tools
     ccat.mad_hatter.uninstall_plugin(plugin_id)
 
