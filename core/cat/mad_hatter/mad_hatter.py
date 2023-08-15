ccf210c6e6b01a7d888be7f7614f5770f21c5de0
Emanuele Morrone <67059270+Pingdred@users.noreply.github.com>
2023-08-15 10:40:14 +0200

    Removed embed_tools from MadHatter

--- core/cat/mad_hatter/mad_hatter.py
+++ core/cat/mad_hatter/mad_hatter.py
@@ -165,51 +165,6 @@ class MadHatter:
         new_setting = Setting(**new_setting)
         crud.upsert_setting_by_name(new_setting)
 
-    # loops over tools and assign an embedding each. If an embedding is not present in vectorDB, it is created and saved
-    def embed_tools(self):
-
-        # retrieve from vectorDB all tool embeddings
-        embedded_tools = self.ccat.memory.vectors.procedural.get_all_points()
-
-        # easy acces to (point_id, tool_description)
-        embedded_tools_ids = [t.id for t in embedded_tools]
-        embedded_tools_descriptions = [t.payload["page_content"] for t in embedded_tools]
-
-        # loop over mad_hatter tools
-        for tool in self.tools:
-            # if the tool is not embedded 
-            if tool.description not in embedded_tools_descriptions:
-                # embed the tool and save it to DB
-                self.ccat.memory.vectors.procedural.add_texts(
-                    [tool.description],
-                    [{
-                        "source": "tool",
-                        "when": time.time(),
-                        "name": tool.name,
-                        "docstring": tool.docstring
-                    }],
-                )
-
-                log(f"Newly embedded tool: {tool.description}", "WARNING")
-        
-        # easy access to mad hatter tools (found in plugins)
-        mad_hatter_tools_descriptions = [t.description for t in self.tools]
-
-        # loop over embedded tools and delete the ones not present in active plugins
-        points_to_be_deleted = []
-        for id, descr in zip(embedded_tools_ids, embedded_tools_descriptions):
-            # if the tool is not active, it inserts it in the list of points to be deleted
-            if descr not in mad_hatter_tools_descriptions:
-                log(f"Deleting embedded tool: {descr}", "WARNING")
-                points_to_be_deleted.append(id)
-
-        # delete not active tools
-        if len(points_to_be_deleted) > 0:
-            self.ccat.memory.vectors.vector_db.delete(
-                collection_name="procedural",
-                points_selector=points_to_be_deleted
-            )
-
     # activate / deactivate plugin
     def toggle_plugin(self, plugin_id):
         log(f"toggle plugin {plugin_id}", "WARNING")
@@ -235,7 +190,6 @@ class MadHatter:
 
             # update cache and embeddings     
             self.sync_hooks_and_tools()
-            self.embed_tools()
 
         else:
             raise Exception("Plugin {plugin_id} not present in plugins folder")
