10629b2386f23a3f571f92e9ef43e339343a7ee8
Piero Savastano <piero.savastano@gmail.com>
2023-09-08 12:53:54 +0200

    move test files

--- core/tests/mad_hatter/test_plugin_extractor.py
+++ core/tests/mad_hatter/test_plugin_extractor.py
@@ -4,7 +4,25 @@ from tests.utils import create_mock_plugin_zip
 from cat.mad_hatter.plugin_extractor import PluginExtractor
 
 
-def test_unpackage(client):
+# zip file does not contain a folder, but the plugin files directly
+def test_unpackage_flat_zip(client):
+
+    plugin_folder = "tests/mocks/mock_plugin_folder"
+    
+    zip_path = create_mock_plugin_zip()
+    extractor = PluginExtractor(zip_path)
+    extracted = extractor.extract(plugin_folder)
+    assert len(extracted) == 1
+    assert extracted[0] == "mock_plugin"
+    assert os.path.exists(f"{plugin_folder}/mock_plugin")
+    assert os.path.exists(f"{plugin_folder}/mock_plugin/mock_tool.py")
+    
+    os.remove(zip_path)
+    shutil.rmtree(f"{plugin_folder}/mock_plugin")
+
+
+# zip file contains just one folder, inside that folder we find the plugin
+def test_unpackage_nested_zip(client):
 
     plugin_folder = "tests/mocks/mock_plugin_folder"
     
