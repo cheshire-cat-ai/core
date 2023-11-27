a0dece06df394939dc3e97d59813e5fe8c1cca36
Piero Savastano <piero.savastano@gmail.com>
2023-11-27 12:56:54 +0100

    delete WorkingMemoryList

--- core/cat/memory/working_memory.py
+++ core/cat/memory/working_memory.py
@@ -50,25 +50,3 @@ class WorkingMemory(dict):
         k = 5
         self["history"] = self["history"][(-k - 1):]
 
-class WorkingMemoryList(dict):
-    """Cat's volatile memory (for all users).
-
-    Handy class that behaves like a `dict` to store temporary custom user data.
-
-    Returns
-    -------
-    dict[str, list]
-        Default instance is a dictionary with `user` key set to a WorkingMemory instance.
-
-    Notes
-    -----
-    The constructor instantiates a dictionary with a `user` key set to a WorkingMemory instance that is further used to
-    reference the anonymous WorkingMemory.
-    """
-
-    def __init__(self):
-        super().__init__(user=WorkingMemory())
-
-    def get_working_memory(self, user_id='user'):
-        self[user_id] = self.get(user_id, WorkingMemory())
-        return self[user_id]
