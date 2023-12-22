5023f71f4381c35a4d428d144b592405aef740ce
Davide Gavio <d.gavio@reply.it>
2023-12-22 10:15:42 +0100

    Added LLMGeminiChatConfig docstring

--- core/cat/factory/llm.py
+++ core/cat/factory/llm.py
@@ -241,6 +241,19 @@ class LLMOllamaConfig(LLMSettings):
     )
 
 class LLMGeminiChatConfig(LLMSettings):
+    """Configuration for the Gemini large language model (LLM).
+
+    This class inherits from the `LLMSettings` class and provides default values for the following attributes:
+
+    * `google_api_key`: The Google API key used to access the Google Natural Language Processing (NLP) API.
+    * `model`: The name of the LLM model to use. In this case, it is set to "gemini".
+    * `temperature`: The temperature of the model, which controls the creativity and variety of the generated responses. 
+    * `top_p`: The top-p truncation value, which controls the probability of the generated words. 
+    * `top_k`: The top-k truncation value, which controls the number of candidate words to consider during generation. 
+    * `max_output_tokens`: The maximum number of tokens to generate in a single response.
+
+    The `LLMGeminiChatConfig` class is used to create an instance of the Gemini LLM model, which can be used to generate text in natural language.
+    """
     google_api_key: str 
     model: str = "gemini"
     temperature: float =  0.1
