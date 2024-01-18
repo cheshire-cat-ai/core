092f6148bc89947824978cc8d979d76d26f26c40
Pingdred <ema.morrone01.em@gmail.com>
2024-01-18 11:52:39 +0100

    Update __init__.py

--- core/cat/mad_hatter/decorators/__init__.py
+++ core/cat/mad_hatter/decorators/__init__.py
@@ -1,3 +1,4 @@
 from cat.mad_hatter.decorators.tool import CatTool, tool
 from cat.mad_hatter.decorators.hook import CatHook, hook
 from cat.mad_hatter.decorators.plugin_decorator import CatPluginDecorator, plugin
+from cat.mad_hatter.decorators.form_decorator import form
