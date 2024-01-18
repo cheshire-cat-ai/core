522a8c061bd4d81885f18380a165304d05054b0a
Pingdred <ema.morrone01.em@gmail.com>
2024-01-18 11:48:00 +0100

    Create cat_form.py

---
+++ core/cat/mad_hatter/cat_form.py
@@ -0,0 +1,49 @@
+from typing import List
+from dataclasses import dataclass
+from pydantic import BaseModel
+
+@dataclass
+class CatFormConfig:
+    # This are only examples of confings, we can define better what is necessary
+    intent_examples: List[str]
+    strict: bool = False
+    return_direct = True
+
+
+class CatForm:  # base model of forms
+
+    # we can also set config attributes directly in CatForm's subclasses withouth using 
+    # a separate config dataclass
+    config: CatFormConfig
+    model: BaseModel = None
+
+    def __init__(self, cat) -> None:
+        self._cat = cat
+
+    def extract(self):
+        """
+        Update the model and check if user want to continue the form
+        """
+        pass
+
+    def is_complete() -> bool:
+        pass
+
+    def next(self):
+        """
+        Ask missing informations
+        """
+        pass
+
+    def submit(self) -> str:
+        """
+        Action
+        """
+        pass
+
+    def clear():
+        """
+        Clear Form on working memory
+        """
+        pass
+
