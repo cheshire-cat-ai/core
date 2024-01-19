94b2b66a64d4db3568fe1fc5c39f537c5400d4fb
Dany <zdanymc@gmail.com>
2024-01-19 19:21:36 +0100

    remove trailing slashes on endpoints

--- core/tests/routes/memory/test_memory_points.py
+++ core/tests/routes/memory/test_memory_points.py
@@ -20,17 +20,17 @@ def test_point_deleted(client):
     assert memory["page_content"] == "Hello Mad Hatter"
 
     # delete point (wrong collection)
-    res = client.delete(f"/memory/collections/wrongcollection/points/{memory['id']}/")
+    res = client.delete(f"/memory/collections/wrongcollection/points/{memory['id']}")
     assert res.status_code == 400
     assert res.json()["detail"]["error"] == "Collection does not exist."
 
     # delete point (wrong id)
-    res = client.delete(f"/memory/collections/episodic/points/wrong_id/")
+    res = client.delete(f"/memory/collections/episodic/points/wrong_id")
     assert res.status_code == 400
     assert res.json()["detail"]["error"] == "Point does not exist."
 
     # delete point (all right)
-    res = client.delete(f"/memory/collections/episodic/points/{memory['id']}/")
+    res = client.delete(f"/memory/collections/episodic/points/{memory['id']}")
     assert res.status_code == 200
     assert res.json()["deleted"] == memory['id']
 
@@ -44,7 +44,7 @@ def test_point_deleted(client):
     assert len(json["vectors"]["collections"]["episodic"]) == 0
 
     # delete again the same point (should not be found)
-    res = client.delete(f"/memory/collections/episodic/points/{memory['id']}/")
+    res = client.delete(f"/memory/collections/episodic/points/{memory['id']}")
     assert res.status_code == 400
     assert res.json()["detail"]["error"] == "Point does not exist."
 
