1a6b4da2aca939f469225ab3651b204db5fa46a8
Nico <nico.pro412@gmail.com>
2024-02-07 09:46:12 +0100

    test updated

--- core/cat/utils.py
+++ core/cat/utils.py
@@ -90,7 +90,8 @@ def get_base_path():
 
 def get_plugins_path():
     """Allows exposing the plugins' path."""
-    return os.path.join(get_base_path(), 'plugins/')
+    # return os.path.join(get_base_path(), 'plugins/')
+    return get_base_path() + 'plugins/'
 
 
 def get_static_url():
