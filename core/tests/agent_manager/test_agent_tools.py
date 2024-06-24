8d2f8a42c927c15e318702058a3d93b85bfbe001
Piero Savastano <piero.savastano@gmail.com>
2024-06-25 01:05:02 +0200

    prepare files to test agent

---
+++ core/tests/agent_manager/test_agent_tools.py
@@ -0,0 +1,10 @@
+import pytest
+
+from tests.agent_manager.agent_fixtures import agent_manager, stray
+
+@pytest.mark.asyncio
+async def test_execute_procedures_agent(agent_manager, stray):
+    assert True  # TODO: this is going to be a mess
+
+# TODO: test tool prompt
+# TODO: test tool output (well and badly formatted)
