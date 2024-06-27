160288ed22210662b02bb2d830b2de3367490a18
Piero Savastano <piero.savastano@gmail.com>
2024-06-28 01:53:12 +0200

    check user credentials on login

--- core/cat/routes/auth.py
+++ core/cat/routes/auth.py
@@ -10,43 +10,23 @@ from fastapi.responses import RedirectResponse
 
 
 # from cat.auth.jwt import create_access_token
+from cat.db.crud import get_users
 from cat.env import get_env
 from cat.routes.static.templates import get_jinja_templates
 
 
 router = APIRouter()
 
-
 class UserCredentials(BaseModel):
     username: str
     password: str
-
 
 class JWTResponse(BaseModel):
     access_token: str
     token_type: str = "bearer"
 
 
-async def authenticate_local_user(username: str, password: str) -> str | None:
-    # authenticate local user credentials
-
-    # check credentials
-    # TODOAUTH: where do we store admin user and pass?
-    if username == "admin" and password == "admin":
-        # TODOAUTH: expiration with timezone needs to be tested
-        # using seconds for easier testing
-        expire_delta_in_seconds = float(get_env("CCAT_JWT_EXPIRE_MINUTES")) * 60
-        expire = datetime.now(utc) + timedelta(seconds=expire_delta_in_seconds)
-        # TODOAUTH: add issuer and redirect_uri (and verify them when a token is validated)
-        return jwt.encode(
-            dict(exp=expire, username=username),
-            get_env("CCAT_JWT_SECRET"),
-            algorithm=get_env("CCAT_JWT_ALGORITHM"),
-        )
-    return None
-
-
-# set cookies and redirect to admin page
+# set cookies and redirect to origin page after login
 @router.post("/redirect")
 async def core_login_token(request: Request, response: Response):
     # get form data from submitted core login form (/auth/core_login)
@@ -77,7 +57,6 @@ async def core_login_token(request: Request, response: Response):
     return response
 
 
-# TODOAUTH to define core login flow
 @router.get("/login")
 async def auth_index(
     request: Request, referer: str = Query(None), retry: int = Query(0)
@@ -119,3 +98,25 @@ async def auth_token(credentials: UserCredentials):
     # wait a little to avoid brute force attacks
     await asyncio.sleep(1)
     raise HTTPException(status_code=403, detail={"error": "Invalid Credentials"})
+
+
+async def authenticate_local_user(username: str, password: str) -> str | None:
+    # authenticate local user credentials
+
+    # brutal search over users, which are stored in a simple JSON list.
+    # waiting to have graph in core to store them properly
+    # TODOAUTH: get rid of this shameful loop
+    users = get_users()
+    for id, user in users.items():
+        if user["username"] == username and user["password"] == password:
+            # TODOAUTH: expiration with timezone needs to be tested
+            # using seconds for easier testing
+            expire_delta_in_seconds = float(get_env("CCAT_JWT_EXPIRE_MINUTES")) * 60
+            expire = datetime.now(utc) + timedelta(seconds=expire_delta_in_seconds)
+            # TODOAUTH: add issuer and redirect_uri (and verify them when a token is validated)
+            return jwt.encode(
+                dict(exp=expire, username=username),
+                get_env("CCAT_JWT_SECRET"),
+                algorithm=get_env("CCAT_JWT_ALGORITHM"),
+            )
+    return None
