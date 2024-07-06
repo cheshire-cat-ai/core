076f22cc65d53b96a894cc3fc19c905796ef5fb3
luca <lucasghegobbi@gmail.com>
2024-07-06 11:36:21 +0200

    support both streaming and not

--- core/cat/looking_glass/callbacks.py
+++ core/cat/looking_glass/callbacks.py
@@ -2,7 +2,6 @@ from typing import Any, Dict, List
 from langchain.callbacks.base import BaseCallbackHandler
 from langchain_core.outputs.llm_result import LLMResult
 import tiktoken
-from cat.log import log
 from cat.convo.messages import ModelInteraction
 
 class NewTokenHandler(BaseCallbackHandler):
@@ -32,18 +31,21 @@ class ModelInteractionHandler(BaseCallbackHandler):
             )
         )
 
+    def _count_tokens(self, text: str) -> int:
+        # cl100k_base is the most common encoding for OpenAI models such as GPT-3.5, GPT-4 - what about other providers?
+        encoding = tiktoken.get_encoding("cl100k_base")
+        return len(encoding.encode(text))
+
 
     def on_llm_start(self, serialized: Dict[str, Any], prompts: List[str], **kwargs) -> None:
-        encoding = tiktoken.get_encoding("cl100k_base")
-        input_tokens = sum(len(encoding.encode(prompt)) for prompt in prompts)
+        input_tokens = sum(self._count_tokens(prompt) for prompt in prompts)
         self.last_interaction.prompt = ''.join(prompts)
         self.last_interaction.input_tokens = input_tokens
 
-    def on_llm_new_token(self, token: str, **kwargs) -> None:
-        self.last_interaction.output_tokens += 1
-
     def on_llm_end(self, response: LLMResult, **kwargs) -> None:
+        self.last_interaction.output_tokens = self._count_tokens(response.generations[0][0].text)
         self.last_interaction.reply = response.generations[0][0].text
+
     @property
     def last_interaction(self) -> ModelInteraction:
         return self.stray.working_memory.model_interactions[-1]
