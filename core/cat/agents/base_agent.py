75125bd62e92cd76614af486f327c49f6b23d416
Piero Savastano <piero.savastano@gmail.com>
2024-07-09 18:33:06 +0200

    remove unused imports

--- core/cat/agents/base_agent.py
+++ core/cat/agents/base_agent.py
@@ -4,7 +4,6 @@ from abc import ABC, abstractmethod
 from langchain_core.utils import get_colored_text
 
 from cat.utils import BaseModelDict
-from cat.log import log
 
 class AgentOutput(BaseModelDict):
     output: str
@@ -25,7 +24,7 @@ class BaseAgent(ABC):
         for m in langchain_prompt.messages:
             print(get_colored_text(type(m).__name__, "green"))
             print(m.content)
-        print(get_colored_text(f"========================================", "green"))
+        print(get_colored_text("========================================", "green"))
         return langchain_prompt
     
     # TODO: this is here to debug langchain, take it away
@@ -33,5 +32,5 @@ class BaseAgent(ABC):
         print("\n")
         print(get_colored_text(f"==================== {title} ====================", "blue"))
         print(langchain_output.content)
-        print(get_colored_text(f"========================================", "blue"))
+        print(get_colored_text("========================================", "blue"))
         return langchain_output
