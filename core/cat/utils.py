cc732db80747330bc9068d73b99d4f536172a6d7
kodaline <doncheva.adelina@gmail.com>
2024-07-24 10:40:08 +0200

    fix: CCAT_CORE_USE_SECURE_PROTOCOLS
    - In compose/env file, set `CCAT_CORE_USE_SECURE_PROTOCOLS=0`
    issue: `get_base_url()` will return `http0://...`
    
    This commit rewrites the condition in order to get the expected
    behavior:
    - `CCAT_CORE_USE_SECURE_PROTOCOLS=1 | true` will give `https://...`
    - everything else will give `http://...`

--- core/cat/utils.py
+++ core/cat/utils.py
@@ -82,9 +82,7 @@ def verbal_timedelta(td: timedelta) -> str:
 
 def get_base_url():
     """Allows exposing the base url."""
-    secure = get_env("CCAT_CORE_USE_SECURE_PROTOCOLS")
-    if secure not in ["", "false", "0"]:
-        secure = "s"
+    secure = "s" if get_env("CCAT_CORE_USE_SECURE_PROTOCOLS") in ("true", "1") else ""
     cat_host = get_env("CCAT_CORE_HOST")
     cat_port = get_env("CCAT_CORE_PORT")
     return f"http{secure}://{cat_host}:{cat_port}/"
