e21703ae8ab82835864683ce34e2adb372297ad3
Pingdred <ema.morrone01.em@gmail.com>
2024-11-06 15:35:15 +0100

    Del: `convert_to_Langchain_message` and `convert_to_Cat_message`
    Removed the `convert_to_Langchain_message` and `convert_to_Cat_message` functions, as they are no longer used in the code base and included in the `StrayCat class`.

--- core/cat/convo/messages.py
+++ core/cat/convo/messages.py
@@ -1,9 +1,10 @@
-from typing import List, Literal
-from cat.utils import BaseModelDict
-from langchain_core.messages import BaseMessage, AIMessage, HumanMessage
-from enum import Enum
-from pydantic import BaseModel, Field, ConfigDict
 import time
+from enum import Enum
+from typing import List, Optional, Literal, Union
+
+from pydantic import BaseModel, Field, ConfigDict, computed_field
+
+from cat.utils import BaseModelDict, deprecation_warning
 
 
 class Role(Enum):
@@ -78,25 +79,3 @@ class UserMessage(BaseModelDict):
     user_id: str
 
 
-def convert_to_Langchain_message(
-        messages: List[UserMessage | CatMessage],
-) -> List[BaseMessage]:
-    messages = []
-    for m in messages:
-        if isinstance(m, CatMessage):
-            messages.append(
-                HumanMessage(content=m.content, response_metadata={"userId": m.user_id})
-            )
-        else:
-            messages.append(
-                AIMessage(content=m.text, response_metadata={"userId": m.user_id})
-            )
-    return messages
-
-
-def convert_to_Cat_message(cat_message: AIMessage, why: MessageWhy) -> CatMessage:
-    return CatMessage(
-        content=cat_message.content,
-        user_id=cat_message.response_metadata["userId"],
-        why=why,
-    )
