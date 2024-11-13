8177c130324ea7027aa2fac9d5e4fbbc9161ec08
Piero Savastano <piero.savastano@gmail.com>
2024-11-13 14:28:13 +0100

    custom endpoints, separate mad_hatter vs network tests

--- core/tests/mad_hatter/test_endpoints.py
+++ core/tests/mad_hatter/test_endpoints.py
@@ -1,66 +1,86 @@
+import pytest
 from cat.mad_hatter.decorators import CustomEndpoint
+from tests.mocks.mock_plugin.mock_endpoint import Item
 
 def test_endpoints_discovery(mad_hatter_with_mock_plugin):
 
+    # discovered endpoints are cached
     mock_plugin_endpoints = mad_hatter_with_mock_plugin.plugins["mock_plugin"].endpoints
-
+    assert mock_plugin_endpoints == mad_hatter_with_mock_plugin.endpoints
+    
+    # discovered endpoints
     assert len(mock_plugin_endpoints) == 4
 
+    # basic properties
     for h in mock_plugin_endpoints:
         assert isinstance(h, CustomEndpoint)
         assert h.plugin_id == "mock_plugin"
 
-        if h.name == "/custom/endpoint":
-            assert h.tags == ["Custom Endpoints"]
-
-        if h.name == "/custom/tests":
-            assert h.tags == ["Tests"]
-
 
 def test_endpoint_decorator(client, mad_hatter_with_mock_plugin):
-
-    response = client.get("/custom/endpoint")
-
-    assert response.status_code == 200
+        
+    endpoint = mad_hatter_with_mock_plugin.endpoints[0]
     
-    assert response.json()["result"] == "endpoint default prefix"
+    assert endpoint.name == "/custom/endpoint"
+    assert endpoint.prefix == "/custom"
+    assert endpoint.path == "/endpoint"
+    assert endpoint.methods == ["GET"]
+    assert endpoint.tags == ["Custom Endpoints"]
+    assert endpoint.function() == {"result":"endpoint default prefix"}
 
 
 def test_endpoint_decorator_prefix(client, mad_hatter_with_mock_plugin):
 
-    response = client.get("/tests/endpoint")
-
-    assert response.status_code == 200
+    endpoint = mad_hatter_with_mock_plugin.endpoints[1]
     
-    assert response.json()["result"] == "endpoint prefix tests"
+    assert endpoint.name == "/tests/endpoint"
+    assert endpoint.prefix == "/tests"
+    assert endpoint.path == "/endpoint"
+    assert endpoint.methods == ["GET"]
+    assert endpoint.tags == ["Tests"]
+    assert endpoint.function() == {"result":"endpoint prefix tests"}
 
 
 def test_get_endpoint(client, mad_hatter_with_mock_plugin):
 
-    response = client.get("/tests/get")
-
-    assert response.status_code == 200
+    endpoint = mad_hatter_with_mock_plugin.endpoints[2]
     
-    assert response.json()["result"] == "ok"
-    assert response.json()["stray_user_id"] == "user"
-
+    assert endpoint.name == "/tests/get"
+    assert endpoint.prefix == "/tests"
+    assert endpoint.path == "/get"
+    assert endpoint.methods == ["GET"]
+    assert endpoint.tags == ["Tests"]
+    # too complicated to simulate the request arguments here,
+    #  see tests/routes/test_custom_endpoints.py
 
 def test_post_endpoint(client, mad_hatter_with_mock_plugin):
 
-    payload = {"name": "the cat", "description" : "it's magic"}
-    response = client.post("/tests/post", json=payload)
+    endpoint = mad_hatter_with_mock_plugin.endpoints[3]
+    
+    assert endpoint.name == "/tests/post"
+    assert endpoint.prefix == "/tests"
+    assert endpoint.path == "/post"
+    assert endpoint.methods == ["POST"]
+    assert endpoint.tags == ["Tests"]
 
-    assert response.status_code == 200
+    payload = Item(name="the cat", description="it's magic")
+    assert endpoint.function(payload) == payload.model_dump()
 
-    assert response.json()["name"] == "the cat"
-    assert response.json()["description"] == "it's magic"
 
-def test_plugin_deactivation(client, mad_hatter_with_mock_plugin):
+@pytest.mark.parametrize("switch_type", ["deactivation", "uninstall"])
+def test_endpoints_deactivation_or_uninstall(switch_type, mad_hatter_with_mock_plugin): 
 
-    response = client.get("/custom/endpoint")
-    assert response.status_code == 200
+    # custom endpoints are registered in mad_hatter
+    for h in mad_hatter_with_mock_plugin.endpoints:
+        assert isinstance(h, CustomEndpoint)
+        assert h.plugin_id == "mock_plugin"
 
-    mad_hatter_with_mock_plugin.toggle_plugin("mock_plugin")
+    if switch_type == "deactivation":
+        # deactivate plugin
+        mad_hatter_with_mock_plugin.toggle_plugin("mock_plugin")
+    else:
+        # uninstall plugin
+        mad_hatter_with_mock_plugin.uninstall_plugin("mock_plugin")
 
-    response = client.get("/custom/endpoint")
-    assert response.status_code == 404
+    # no more custom endpoints
+    assert mad_hatter_with_mock_plugin.endpoints == []
