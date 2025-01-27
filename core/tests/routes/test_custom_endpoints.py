ecaed9f70edd0677f72f08024b46019ca2dacad3
samadpls <abdulsamadsid1@gmail.com>
2025-01-28 02:32:31 +0500

    Add PUT and DELETE endpoint decorators with tests
    Signed-off-by: samadpls <abdulsamadsid1@gmail.com>

--- core/tests/routes/test_custom_endpoints.py
+++ core/tests/routes/test_custom_endpoints.py
@@ -31,6 +31,21 @@ def test_custom_endpoint_post(client, just_installed_plugin):
     assert response.json()["name"] == "the cat"
     assert response.json()["description"] == "it's magic"
 
+def test_custom_endpoint_put(client, just_installed_plugin):
+    payload = {"name": "the cat", "description": "it's magic"}
+    response = client.put("/tests/crud", json=payload)
+    
+    assert response.status_code == 200
+    assert response.json()["name"] == "the cat"
+    assert response.json()["description"] == "it's magic"
+
+def test_custom_endpoint_delete(client, just_installed_plugin):
+    response = client.delete("/tests/crud/123")
+    
+    assert response.status_code == 200
+    assert response.json()["result"] == "ok"
+    assert response.json()["deleted_id"] == 123
+
 
 @pytest.mark.parametrize("switch_type", ["deactivation", "uninstall"])
 def test_custom_endpoints_on_plugin_deactivation_or_uninstall(
